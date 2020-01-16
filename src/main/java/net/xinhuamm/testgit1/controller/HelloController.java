b1a804fb1bb2cfafd9e59706a32781608bc9149e
qinixiangyang111 <SpringF1_Qin@163.com>
2020-01-16 11:40:41 +0800

    qrqewr

---
+++ src/main/java/net/xinhuamm/testgit1/controller/HelloController.java
@@ -0,0 +1,26 @@
+package net.xinhuamm.testgit1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(value = "/demo",method = RequestMethod.GET)
+    public String  getHello(){
+        System.out.println("Hello World");
+        return  "HelloWorld!!";
+    }
+
+    @RequestMapping(value = "/pp",method = RequestMethod.POST)
+    public String  getPost(){
+        System.out.println("Hello World");
+        return  "HelloWorld!!";
+    }
+}
