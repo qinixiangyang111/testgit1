71fe7f3c39d8d19233b151f0d28c1c9521ad0252
qinixiangyang111 <SpringF1_Qin@163.com>
2020-01-16 10:38:42 +0800

    qrqewr

---
+++ src/test/java/net/xinhuamm/testgit1/Testgit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xinhuamm.testgit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testgit1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
